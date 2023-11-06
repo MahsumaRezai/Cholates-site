63865c24ce8d84d7f45bf4bf0907eca39dde0533
unknown <mahsuma.rezai8@gmail.com>
2023-11-06 02:57:47 -0800

    first commit for new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import React from 'react'
+
+
+function App(props) {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default App;
