1064a519d7158477a1dc6e7d2a17e2360cf33b00
unknown <mahsuma.rezai8@gmail.com>
2023-11-06 06:59:01 -0800

    add about page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -8,9 +8,16 @@ const Home = (props) => {
     return (
         <Fragment>
             <Card>
-                <div>
-                    <img src={img} alt="" className={classes.img} />
-                    <h1>Chocolate</h1>
+                <div className={classes.home}>
+                    <div className={classes.images}>
+                        <img src={img} alt="" className={classes.img} />
+
+                    </div>
+                    <div className={classes.title}>
+                        <h1 className={classes.titles}>Chocolate</h1>
+                        <p className={classes.text}>Chocolate is very amzing and all people around world like it <br></br> and this is very good for people</p>
+
+                    </div>
                 </div>
 
             </Card>
