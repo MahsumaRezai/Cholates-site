d8e1bf6b03e4e25d6c4683d03adb71301dd0fb60
unknown <mahsuma.rezai8@gmail.com>
2023-11-06 05:56:37 -0800

    add home style page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,18 @@
 import { Fragment } from "react";
 import Card from './Card.js';
+import img from './images/slider-img.png';
+import classes from './Home.module.css'
+
 
 const Home = (props) => {
     return (
         <Fragment>
             <Card>
-                <h2>hi</h2>
+                <div>
+                    <img src={img} alt="" className={classes.img} />
+                    <h1>Chocolate</h1>
+                </div>
+
             </Card>
 
         </Fragment>
