3606be65d48ddf3dcd51d4d37e2b147c0cedaca8
unknown <mahsuma.rezai8@gmail.com>
2023-11-06 07:07:38 -0800

    add about text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import React from 'react';
 import Home from "./Component/Home.js";
+import About from "./Component/About.js";
 
 
 function App(props) {
     return (
         <Fragment>
             <Home />
-            
+            <About />
         </Fragment>
     )
 }
---
+++ src/Component/About.js
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import classes from './About.module.css'
+
+const About = (props) => {
+    return (
+        <Fragment>
+            <div className={classes.about}>
+                <div>
+                    <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                         as opposed to using 'Content here, content here', making it look like readable English.
+                         Many desktop publishing packages and web pagend web page editors now use Lorem Ipsum as their default model text,</p>
+
+                </div>
+                <div>
+
+                </div>
+
+
+            </div>
+
+        </Fragment>
+    )
+}
+export default About;
