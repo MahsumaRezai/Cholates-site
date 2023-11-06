7f1d9743cf6e9e65fac9b4f9c1ed3cdd279e9333
unknown <mahsuma.rezai8@gmail.com>
2023-11-06 07:26:06 -0800

    add about style

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,17 +1,23 @@
 import { Fragment } from "react";
-import classes from './About.module.css'
+
+import classes from './About.module.css';
+import img from './images/about-img.png'
 
 const About = (props) => {
     return (
         <Fragment>
             <div className={classes.about}>
-                <div>
-                    <p>Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-                         as opposed to using 'Content here, content here', making it look like readable English.
-                         Many desktop publishing packages and web pagend web page editors now use Lorem Ipsum as their default model text,</p>
+                <div className={classes.texts}>
+                    <h2 className={classes.title}>About Our Cholates</h2>
+                    <p className={classes.para}>Lorem Ipsum is that it has a more-or-less normal distribution of letters,<br></br>
+                        as opposed to using 'Content here, content here', making it look like readable English.<br></br>
+                        Many desktop publishing packages and web pagend web page editors now use Lorem Ipsum as their default
+                         model text,</p>
+
 
                 </div>
                 <div>
+                    <img alt="" src={img} />
 
                 </div>
 
