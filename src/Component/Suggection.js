aa77ff5329c8ab52a6787596ad92cb55a9056184
unknown <mahsuma.rezai8@gmail.com>
2023-11-08 03:11:24 -0800

    add last commit for todya 8 nov

--- src/Component/Suggection.js
+++ src/Component/Suggection.js
@@ -1,18 +1,20 @@
 import { Fragment } from "react";
 import Button from "./Button";
 import classes from './Button.module.css';
-
+import img from './images/offer-img.png'
 
 
 const Suggection = (props) => {
     return (
         <Fragment>
             <div className={classes.content}>
-                <h1>Offers on chocolates</h1>
-                <h2>Get 5% Offer</h2>
-                <h3>any Chocolate items</h3>
+                <h1 className={classes.title}>Offers on chocolates</h1>
+                <p className={classes.text}>Get 5% Offer <br></br>any Chocolate items</p>
                 <Button />
+                <img src={img} alt="" className={classes.assest} />
+
             </div>
+
 
         </Fragment>
     )
