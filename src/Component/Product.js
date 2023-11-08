3db7429f40d9d6a9292747b7dd06f5bd54bc7d86
unknown <mahsuma.rezai8@gmail.com>
2023-11-07 23:05:12 -0800

    add all product

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -3,34 +3,30 @@ import classes from './pro.module.css';
 import img2 from './images/chocolate1.png';
 import img3 from './images/chocolate2.png';
 import img4 from './images/chocolate3.png';
-import img1 from './images/client-chocolate.png';
 
 const Product = (props) => {
     return (
         <Fragment>
             <div className={classes.product}>
                 <div className={classes.products}>
-                    <div>
-                        <img src={img1} alt="" className={classes.img} />
-                    </div>
-                    <div>
-                        <div>
-                            <img src={img2} alt="" className={classes.images} />
+                    <h1 className={classes.title}>Our chocolate products</h1>
 
-                        </div>
-                        <div>
-                            <img src={img3} alt="" className={classes.images} />
-
-                        </div>
-                        <div>
-                            <img src={img4} alt="" className={classes.images} />
-
-                        </div>
+                    <div className={classes.pro}>
+                        <img src={img2} alt="" className={classes.images} />
 
                     </div>
+                    <div className={classes.pro}>
+                        <img src={img3} alt="" className={classes.images} />
+
+                    </div>
+                    <div className={classes.pro}>
+                        <img src={img4} alt="" className={classes.images} />
+
+                    </div>
+
                 </div>
-
             </div>
+
 
         </Fragment>
     )
