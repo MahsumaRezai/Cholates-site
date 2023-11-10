a34c79e9883be6e0ae9821137c520fdf954fa2e5
unknown <mahsuma.rezai8@gmail.com>
2023-11-09 23:29:30 -0800

    add last commit for today

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,8 +1,20 @@
 import { Fragment } from 'react';
 import img from './images/client-chocolate.png'
 import classes from './Form.module.css';
+import { useState } from 'react';
 
 const Form = (props) => {
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [massage, setMassage] = useState('');
+
+    const FormHandler=(event)=>{
+        event.preventDefault('');
+        console.log(name,phone,email,massage)
+    }
+
+
     return (
         <Fragment>
             <div className={classes.content}>
@@ -10,30 +22,30 @@ const Form = (props) => {
                     <img src={img} alt="" className={classes.img} />
                 </div>
 
-                <div className={classes.form}>
+                <form className={classes.form} onSubmit={FormHandler}>
                     <h1>Contact Us</h1>
                     <div>
-                        <input type="text" placeholder="Full Name " />
+                        <input type="text" placeholder="Full Name " onChange={setName} />
                     </div>
                     <div>
-                        <input type="text" placeholder="Phone number" />
+                        <input type="text" placeholder="Phone number" onChange={setPhone} />
                     </div>
                     <div>
-                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Email"  onChange={setEmail}/>
                     </div>
                     <div>
-                        <input type="text" class="message-box" placeholder="Message" />
+                        <input type="text" class="message-box" placeholder="Message"  onChange={setMassage}/>
                     </div>
                     <div>
                         <button className={classes.button}>
                             <span className={classes.btn}>
                                 SEND NOW
-    
+
                             </span>
                         </button>
-                        
+
                     </div>
-                </div>
+                </form>
             </div>
 
 
